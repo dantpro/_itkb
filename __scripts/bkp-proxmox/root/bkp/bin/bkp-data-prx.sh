b873fe2a95440f56efcd45d988cf39ae0c3e636e
Dmitry Antonov <dant@dant.pro>
2025-04-07 17:41:03 +0700

    something was changed

--- __scripts/bkp-proxmox/root/bkp/bin/bkp-data-prx.sh
+++ __scripts/bkp-proxmox/root/bkp/bin/bkp-data-prx.sh
@@ -9,6 +9,7 @@ BKP_SYNC_DIR="/mnt/bkpn/$(hostname)"
 BKP_DB_DST_DIR="/root/bkp/dat/db"
 
 BKP_TIME=`date +%y%m%d_%H%M%S`
+
 HOST=`hostname`
 
 KEEP_FILES=10
@@ -24,7 +25,6 @@ tar -cvzp -T $BKP_LST -f $BKP_DST_DIR/$BKP_TIME\_$HOST\_data.tgz
 # DB Backup
 sqlite3 /var/lib/pve-cluster/config.db .dump |gzip -c > $BKP_DB_DST_DIR/config\_db\_dump\_$BKP_TIME.sql.gz
 
-
 printf "\n\nDelete old files\n..."
 find $BKP_DST_DIR/ -type f -name "*_*_*_data.tgz"  -printf '%T@ "%p"\n' | \
 	sort -n | \
@@ -38,8 +38,8 @@ find $BKP_DB_DST_DIR/ -type f -name "config_db_dump_*.sql.gz"  -printf '%T@ "%p"
 	awk '{print $2}' | \
 	xargs -I {} rm {}
 
-
 printf "\n\nRsyncing backup dir to remote location\n...\n\n"
+
 rsync -rlptDv --progress --delete $BKP_DST_DIR/ $BKP_SYNC_DIR
 
 printf "\n\n"
@@ -47,3 +47,4 @@ printf "\n\n"
 ls -l $BKP_DST_DIR/ $BKP_SYNC_DIR/
 
 umount /mnt/bkpn
+
